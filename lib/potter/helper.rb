f770d64d526e8a5b9ff6c2fd30d7c73baf6ae435
Dexter Ogie-osahon <dexter.ogie-osahon@live.com>
2012-06-02 19:16:49 +0300

    photo

--- lib/potter/helper.rb
+++ lib/potter/helper.rb
@@ -1,6 +1,6 @@
 require 'active_support/concern'
 
-%w{likeable liker like_store}.each do |f|
+%w{likeable liker like_store photographable photographer photograph_store}.each do |f|
   require "#{File.dirname(__FILE__)}/#{f}"
 end
 
@@ -9,7 +9,7 @@ module Potter
     extend ActiveSupport::Concern
 
     module ClassMethods
-       ## Like
+      ## Like
       def acts_as_liker(opts = nil)
         include Potter::Liker
       end
@@ -22,6 +22,18 @@ module Potter
         include Potter::LikeStore
       end
 
+      ## Album
+      def acts_as_photographer(opts = nil)
+        include Potter::Photographer
+      end
+
+      def acts_as_photographable(opts = nil)
+        include Potter::Photographable
+      end
+
+      def acts_as_photograph_store(opts = nil)
+        include Potter::PhotographStore
+      end
     end
   end
 end

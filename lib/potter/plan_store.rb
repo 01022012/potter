70964478ce9dcb435edf874fc23d90df1bd31d5b
Dexter Ogie-osahon <dexter.ogie-osahon@live.com>
2012-06-03 17:08:29 +0300

    plan-

--- lib/potter/plan_store.rb
+++ lib/potter/plan_store.rb
@@ -6,7 +6,7 @@ module Potter
       belongs_to :planer,   :polymorphic => true
       belongs_to :planable, :polymorphic => true
 
-      
+      attr_accessor :planable_name, :planable_zone, :planable_address, :planable_city, :planable_description, :planable_discount, :planable_reservation_option, :planable_done, :planable_event_date, :planable_removed
       validates_presence_of :planable_type, :scope => [:planable_id, :planer_type, :planer_id, :planable_name, :planable_zone, :planable_address, :planable_city, :planable_description, :planable_discount, :planable_reservation_option, :planable_done, :planable_event_date, :planable_removed], :message => 'You cannot make a plan without activities.'
 
       validates_presence_of :planable_name, :planable_zone, :planable_address, :planable_city, :planable_description, :planable_discount, :planable_reservation_option, :planable_done, :planable_event_date, :planable_removed
